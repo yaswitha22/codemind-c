fba826a1cb45446fcc84e57c2f97d5097186b451
Yaswitha <97159770+yaswitha22@users.noreply.github.com>
2022-04-20 09:48:10 +0530

    #include<stdio.h> int main() {     int n,arr[100],i,c=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%2==1&&i%2==0)         {             c=1;             printf("False");             break;         }     }     if(c==0)     {         printf("True");     } }

---
+++ Strictly_ODD.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[100],i,c=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2==1&&i%2==0)
+        {
+            c=1;
+            printf("False");
+            break;
+        }
+    }
+    if(c==0)
+    {
+        printf("True");
+    }
+}
