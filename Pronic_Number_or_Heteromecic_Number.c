1116a1bdab7e1324941ac98c99befcf384a5b54e
Yaswitha <97159770+yaswitha22@users.noreply.github.com>
2022-04-20 20:38:14 +0530

    #include<stdio.h> int main() {     int n,i,c=0;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         if(i*(i+1)==n)         {             printf("YES");             c++;             break;         }     }     if(c==0)     {         printf("NO");     } }

---
+++ Pronic_Number_or_Heteromecic_Number.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,c=0;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        if(i*(i+1)==n)
+        {
+            printf("YES");
+            c++;
+            break;
+        }
+    }
+    if(c==0)
+    {
+        printf("NO");
+    }
+}
