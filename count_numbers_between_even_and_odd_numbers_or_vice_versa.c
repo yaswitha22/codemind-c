b3fc40d088c7ee2810155014f864009610ea4c51
Yaswitha <97159770+yaswitha22@users.noreply.github.com>
2022-05-03 22:33:31 +0530

    #include<stdio.h> int main() {     int arr[100],i,c=0,n;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n-2;i++)     {         if(arr[i]%2==0&&arr[i+2]%2==1||arr[i]%2==1&&arr[i+2]%2==0)         {             if(arr[i+1]%2==0||arr[i+1]%2==1)             {                 c++;             }         }     }     printf("%d",c); }

---
+++ count_numbers_between_even_and_odd_numbers_or_vice_versa.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int arr[100],i,c=0,n;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n-2;i++)
+    {
+        if(arr[i]%2==0&&arr[i+2]%2==1||arr[i]%2==1&&arr[i+2]%2==0)
+        {
+            if(arr[i+1]%2==0||arr[i+1]%2==1)
+            {
+                c++;
+            }
+        }
+    }
+    printf("%d",c);
+}
