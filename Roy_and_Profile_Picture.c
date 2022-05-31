d0bddfb63a1ed1b5acbd94cffae1e0dc28e42294
Yaswitha <97159770+yaswitha22@users.noreply.github.com>
2022-05-31 19:44:18 +0530

    #include<stdio.h> int main() {     int l,n,w,h,i;     scanf("%d%d",&l,&n);     for(i=1;i<=n;i++)     {         scanf("%d %d",&w,&h);     if(w<l || h<l)     {         printf("UPLOAD ANOTHER ");     }     else if(w>=l && h>=l)     {         if(w==h)         {         printf("ACCEPTED ");         }         else         {            printf("CROP IT ");         }     }     }     return 0; }

---
+++ Roy_and_Profile_Picture.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int l,n,w,h,i;
+    scanf("%d%d",&l,&n);
+    for(i=1;i<=n;i++)
+    {
+        scanf("%d %d",&w,&h);
+    if(w<l || h<l)
+    {
+        printf("UPLOAD ANOTHER
+");
+    }
+    else if(w>=l && h>=l)
+    {
+        if(w==h)
+        {
+        printf("ACCEPTED
+");
+        }
+        else 
+        {
+           printf("CROP IT
+");
+        }
+    }
+    }
+    return 0;
+}
