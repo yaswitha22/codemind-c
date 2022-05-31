bd9e098b25525be6d11bc71bd1a4a1242799e7e0
Yaswitha <97159770+yaswitha22@users.noreply.github.com>
2022-05-31 18:02:26 +0530

    #include<stdio.h> int main() {     int n,arr[100],i,c=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]==0 || arr[i]==1)         {             c++;         }     }     if(c==n)     {         printf("True");     }     else     {         printf("False");     } }

---
+++ Binary_Array.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[100],i,c=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]==0 || arr[i]==1)
+        {
+            c++;
+        }
+    }
+    if(c==n)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
