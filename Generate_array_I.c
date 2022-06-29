5bc1b1317bf7325fbbbb4f6ebb660392fc29c3ae
Yaswitha <97159770+yaswitha22@users.noreply.github.com>
2022-06-29 23:19:35 +0530

    #include<stdio.h> int main() {     int n,arr[100],i,j,c=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(i%2==0)         {         for(j=1;j<=arr[i+1];j++)         {             printf("%d ",arr[i]);         }         }     } }

---
+++ Generate_array_I.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[100],i,j,c=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(i%2==0)
+        {
+        for(j=1;j<=arr[i+1];j++)
+        {
+            printf("%d ",arr[i]);
+        }
+        }
+    }
+}
