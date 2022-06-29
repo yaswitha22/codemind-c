f01e2ad79ab548a62dc227ad4700a303250ea49b
Yaswitha <97159770+yaswitha22@users.noreply.github.com>
2022-06-30 00:02:43 +0530

    #include<stdio.h> int main() {     int n,arr[100],i,j;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }      for(i=0;i<n;i++)     {         if(arr[i]%2==0)         {             printf("%d ",arr[i]);         }     }     for(i=0;i<n;i++)     {         if(arr[i]%2!=0)         {             printf("%d ",arr[i]);         }     }     return 0; }

--- even_elements_followed_by_odd.c
+++ even_elements_followed_by_odd.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 int main()
 {
-    int n,arr[100],i;
+    int n,arr[100],i,j;
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
         scanf("%d",&arr[i]);
     }
-    for(i=0;i<n;i++)
+     for(i=0;i<n;i++)
     {
         if(arr[i]%2==0)
         {
@@ -16,9 +16,10 @@ int main()
     }
     for(i=0;i<n;i++)
     {
-        if(arr[i]%2)
+        if(arr[i]%2!=0)
         {
             printf("%d ",arr[i]);
         }
     }
+    return 0;
 }
