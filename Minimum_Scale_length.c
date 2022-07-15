696bf331ed243a05341cc76659aad485ed0376ec
Yaswitha <97159770+yaswitha22@users.noreply.github.com>
2022-07-15 11:40:35 +0530

    #include<stdio.h> int main() {     int n,i,arr[100],j,min=9999,res=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {        scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {        if(min>arr[i])        {            min=arr[i];        }     }     for(i=min;i>=1;i--)     {       res=0;       for(j=0;j<n;j++)       {           if(arr[j]%i!=0)           {               res=1;               break;           }       }       if(res==0)       {           printf("%d",i);           break;       }     } }

---
+++ Minimum_Scale_length.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,arr[100],j,min=9999,res=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+       scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+       if(min>arr[i])
+       {
+           min=arr[i];
+       }
+    }
+    for(i=min;i>=1;i--)
+    {
+      res=0;
+      for(j=0;j<n;j++)
+      {
+          if(arr[j]%i!=0)
+          {
+              res=1;
+              break;
+          }
+      }
+      if(res==0)
+      {
+          printf("%d",i);
+          break;
+      }
+    }
+}
