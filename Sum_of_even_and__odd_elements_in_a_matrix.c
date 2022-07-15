1d189eef7ea236045ce0c0f0f8b485525643f3cc
Yaswitha <97159770+yaswitha22@users.noreply.github.com>
2022-07-15 10:05:17 +0530

    #include<stdio.h> int main() {     int n,m,i,j,a[100][100],os=0,es=0;     scanf("%d%d",&n,&m);     for(i=0;i<n;i++)     {         for(j=0;j<m;j++)         {         scanf("%d",&a[i][j]);         }     }     for(i=0;i<n;i++)     {         for(j=0;j<m;j++)         {          if (a[i][j]%2==0)          {              es+=a[i][j];          }          else          {              os+=a[i][j];          }         }     }     printf("%d %d",es,os);
    }

--- Sum_of_even_and__odd_elements_in_a_matrix.c
+++ Sum_of_even_and__odd_elements_in_a_matrix.c
@@ -1,28 +1,29 @@
 #include<stdio.h>
 int main()
 {
-    int a[100][100],i,j,n,m,es=0,os=0;
+    int n,m,i,j,a[100][100],os=0,es=0;
     scanf("%d%d",&n,&m);
     for(i=0;i<n;i++)
     {
         for(j=0;j<m;j++)
         {
-            scanf("%d",&a[i][j]);
+        scanf("%d",&a[i][j]);
         }
     }
     for(i=0;i<n;i++)
     {
         for(j=0;j<m;j++)
         {
-            if(a[i][j]%2==0)
-            {
-                es+=a[i][j];
-            }
-            else
-            {
-                os+=a[i][j];
-            }
+         if (a[i][j]%2==0)
+         {
+             es+=a[i][j];
+         }
+         else
+         {
+             os+=a[i][j];
+         }
         }
     }
     printf("%d %d",es,os);
+    
 }
