2a952c77c178e4900716827758aca42731ece707
Yaswitha <97159770+yaswitha22@users.noreply.github.com>
2022-10-27 11:21:00 +0530

    #include<stdio.h> int main() {     int n,arr[100],i,j,c=0,count=0;     scanf("%d",&n);     for(i=0;i<n;i++)     scanf("%d",&arr[i]);     for(i=0;i<n;i++)     {         c=0;         if(arr[i]==-1)         continue;         else         {         for(j=0;j<n;j++)         {             if(arr[i]==arr[j] && i!=j)             {                 arr[j]=-1;                 c++;             }         }         }         if((c+1)>=2)         count+=((int)(c+1)/2);     }     printf("%d",count); }

---
+++ CLOTHING_STORE.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[100],i,j,c=0,count=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    scanf("%d",&arr[i]);
+    for(i=0;i<n;i++)
+    {
+        c=0;
+        if(arr[i]==-1)
+        continue;
+        else
+        {
+        for(j=0;j<n;j++)
+        {
+            if(arr[i]==arr[j] && i!=j)
+            {
+                arr[j]=-1;
+                c++;
+            }
+        }
+        }
+        if((c+1)>=2)
+        count+=((int)(c+1)/2);
+    }
+    printf("%d",count);
+}
